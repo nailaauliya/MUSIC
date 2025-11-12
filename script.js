fca5ecf284d60ff60c3528e081e67941b7f0da1a
nailaauliya <naila.auliyap@gmail.com>
2025-11-12 17:43:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const player = document.querySelector(".player");
 const songInfo = document.createElement("div");
 songInfo.classList.add("song-info");
 songInfo.innerHTML = `
-  <img id="cover" src="musiccover1.jpg" alt="cover">
+  <img id="cover" src="img/musiccover1.jpg" alt="cover">
   <div class="text">
     <h3 id="song-title">No song playing</h3>
     <p id="lyrics">Select a song to see lyrics...</p>
@@ -23,42 +23,42 @@ player.appendChild(audio);
 const songData = {
   "number one girl – ROSE": {
     src: "songs/number-one-girl.mp3",
-    cover: "rosie.jpg",
+    cover: "img/rosie.jpg",
     lyric: "Your one and only<br>So what's it gon' take for you to want me?<br>I'd give it all up if you told me that I'd be<br>The number one girl in your eyes"
   },
   "3AM – ROSE": {
     src: "songs/3am.mp3",
-    cover: "rosie.jpg",
+    cover: "img/rosie.jpg",
     lyric: "Late night thoughts, I'm still awake<br>Counting stars until the day breaks"
   },
   "GO! – CORTIZ": {
     src: "songs/go.mp3",
-    cover: "cortiz.jpg",
+    cover: "img/cortiz.jpg",
     lyric: "Run and chase it, don’t ever slow<br>Keep it wild, just let it go"
   },
   "JUMP – Blackpink": {
     src: "songs/jump.mp3",
-    cover: "bp - Copy.jpg",
+    cover: "img/bp.jpg",
     lyric: "Jump high, don’t look down<br>We’re the queens, wearing the crown"
   },
   "'Cause You Have To – LANY": {
     src: "songs/cause-you-have-to.mp3",
-    cover: "lany.jpg",
+    cover: "img/lany.jpg",
     lyric: "I still see your face in every crowd<br>Cause you have to, but I don’t know how"
   },
   "drink or coffe – ROSE": {
     src: "songs/drink-or-coffee.mp3",
-    cover: "rosie.jpg",
+    cover: "img/rosie.jpg",
     lyric: "Coffee cups and rainy nights<br>Still thinking of you under city lights"
   },
   "Rich Man – aespa": {
     src: "songs/rich-man.mp3",
-    cover: "aespa.png",
+    cover: "img/aespa.png",
     lyric: "Money talks, but love’s the sound<br>I keep my feet on solid ground"
   },
   "XXL – LANY": {
     src: "songs/xxl.mp3",
-    cover: "lany.jpg",
+    cover: "img/lany.jpg",
     lyric: "Dreams too big, hearts too wide<br>We’re living loud, no need to hide"
   }
 };
